353f61ead1cf5e0d0a6f0297ccd5bcb1500d3a39
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-05-04 16:55:03 -0700

    fix shape of python results

--- search-website/api/Search/__init__.py
+++ search-website/api/Search/__init__.py
@@ -53,6 +53,50 @@ def create_filter_expression(filter_list, facets):
 
     return separator.join(filter_expressions)
 
+def new_shape(docs):
+    
+    old_api_shape = list(docs)
+    
+    count=0
+    client_side_expected_shape = []
+    
+    for item in old_api_shape:
+        
+        new_document = {}
+        new_document["score"]=item["@search.score"]
+        new_document["highlights"]=item["@search.highlights"]
+
+        new_shape = {}
+        new_shape["id"]=item["id"]
+        new_shape["goodreads_book_id"]=item["goodreads_book_id"]
+        new_shape["best_book_id"]=item["best_book_id"]
+        new_shape["work_id"]=item["work_id"]        
+        new_shape["books_count"]=item["books_count"]        
+        new_shape["isbn"]=item["isbn"]
+        new_shape["isbn13"]=item["isbn13"]
+        new_shape["authors"]=item["authors"]
+        new_shape["original_publication_year"]=item["original_publication_year"]
+        new_shape["original_title"]=item["original_title"]
+        new_shape["title"]=item["title"]
+        new_shape["language_code"]=item["language_code"]
+        new_shape["average_rating"]=item["average_rating"]
+        new_shape["ratings_count"]=item["ratings_count"]
+        new_shape["work_ratings_count"]=item["work_ratings_count"]
+        new_shape["work_text_reviews_count"]=item["work_text_reviews_count"]
+        new_shape["ratings_1"]=item["ratings_1"]
+        new_shape["ratings_2"]=item["ratings_2"]
+        new_shape["ratings_3"]=item["ratings_3"]
+        new_shape["ratings_4"]=item["ratings_4"]
+        new_shape["ratings_5"]=item["ratings_5"]
+        new_shape["image_url"]=item["image_url"]
+        new_shape["small_image_url"]=item["small_image_url"]
+        
+        new_document["document"]=new_shape
+        
+        client_side_expected_shape.append(new_document)
+    
+    return json.dumps(list(client_side_expected_shape))
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # variables sent in body
@@ -71,6 +115,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         
         search_results = search_client.search(search_text=q, top=top,skip=skip, facets=facetKeys, filter=filter, include_total_count=True)
         
+        returned_docs = new_shape(search_results)
         returned_count = search_results.get_count()
         returned_facets = search_results.get_facets()
         
