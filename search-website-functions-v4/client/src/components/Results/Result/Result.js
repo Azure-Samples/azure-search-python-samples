484cb87d5dd786fec52e1bdad0a0c32f5233e64d
HernanOlave <hernan.olave.b@gmail.com>
2023-08-21 17:17:39 -0400

    changed display content

--- search-website-functions-v4/client/src/components/Results/Result/Result.js
+++ search-website-functions-v4/client/src/components/Results/Result/Result.js
@@ -4,14 +4,12 @@ import './Result.css';
 
 export default function Result(props) {
     
-    console.log(`result prop = ${JSON.stringify(props)}`)
-    
     return(
     <div className="card result">
         <a href={`/details/${props.document.id}`}>
-            <img className="card-img-top" src={props.document.image_url} alt={props.document.original_title}></img>
+            <h6 className="title-style">{props.document.filename}</h6>
             <div className="card-body">
-                <h6 className="title-style">{props.document.original_title}</h6>
+                <p>{props.document.content.substring(0, 300)} ...</p>
             </div>
         </a>
     </div>
