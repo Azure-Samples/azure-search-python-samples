2c784d9de3ed8918ca962923c6a24261643081c5
JimacoMS4 <84105740+JimacoMS4@users.noreply.github.com>
2023-09-20 15:59:21 -0700

    incorporate reviewer feedback

--- search-website-functions-v4/api-v4/suggest.py
+++ search-website-functions-v4/api-v4/suggest.py
@@ -32,17 +32,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # variables sent in body
     req_body = req.get_json()
     q = req_body.get("q")
-    top = req_body.get("top")
-    suggester = req_body.get("suggester")
+    top = req_body.get("top") or 5
+    suggester = req_body.get("suggester") or "sg"
 
     if q:
-        logging.info(f"/Suggest q = {q}")
-        suggestions = search_client.suggest(search_text=q, suggester_name="sg", top=5)
+        logging.info("/Suggest q = %s", q)
+        suggestions = search_client.suggest(search_text=q, suggester_name=suggester, top=top)
 
         # format the React app expects
         full_response = {}
         full_response["suggestions"] = suggestions
-        print(suggestions)
+        logging.debug(suggestions)
 
         return func.HttpResponse(
             body=json.dumps(full_response), mimetype="application/json", status_code=200
