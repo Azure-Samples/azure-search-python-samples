a9f646febc7c069a948ccc5de8fcb95914fd0dcf
HongyiInstalily <134713257+HongyiInstalily@users.noreply.github.com>
2024-03-25 21:37:13 -0400

    test suggester

--- search-website-functions-v4/api/suggest.py
+++ search-website-functions-v4/api/suggest.py
@@ -17,8 +17,40 @@ service_name = environment_vars["search_service_name"]
 endpoint = f"https://{service_name}.search.windows.net"
 key = environment_vars["search_api_key"]
 
+# # Your index name
+# index_name = "good-books"
+
+# # Create Azure SDK client
+# search_client = SearchClient(endpoint, index_name, AzureKeyCredential(key))
+
+
+# bp=func.Blueprint()
+# @bp.function_name("suggest")
+# @bp.route(route="suggest", methods=[func.HttpMethod.GET, func.HttpMethod.POST] )
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+
+#     # variables sent in body
+#     req_body = req.get_json()
+#     q = req_body.get("q")
+#     top = req_body.get("top") or 5
+#     suggester = req_body.get("suggester") or "sg"
+
+#     if q:
+#         logging.info("/Suggest q = %s", q)
+#         suggestions = search_client.suggest(search_text=q, suggester_name=suggester, top=top)
+
+#         # format the React app expects
+#         full_response = {}
+#         full_response["suggestions"] = suggestions
+#         logging.debug(suggestions)
+
+#         return func.HttpResponse(
+#             body=json.dumps(full_response), mimetype="application/json", status_code=200
+#         )
+#     else:
+#         return func.HttpResponse("No query param found.", status_code=200)
 # Your index name
-index_name = "good-books"
+index_name = "partselect"
 
 # Create Azure SDK client
 search_client = SearchClient(endpoint, index_name, AzureKeyCredential(key))
@@ -37,8 +69,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if q:
         logging.info("/Suggest q = %s", q)
-        suggestions = search_client.suggest(search_text=q, suggester_name=suggester, top=top)
-
+        suggestions = search_client.suggest(search_text=q, suggester_name="modelsuggest", top=top)
+        
         # format the React app expects
         full_response = {}
         full_response["suggestions"] = suggestions
