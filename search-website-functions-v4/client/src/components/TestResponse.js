12b06a735aef123581717c37aa84d3af65d341a2
Prashanthi Ramachandran <prashanthi@instalily.ai>
2024-03-26 10:55:22 -0400

    add test search resp

---
+++ search-website-functions-v4/client/src/components/TestResponse.js
@@ -0,0 +1,321 @@
+export const searchResponse = {
+    "count": 70,
+    "facets": {
+        "authors": [
+            {
+                "value": "Michael Connelly",
+                "count": 26
+            },
+            {
+                "value": "J.K. Rowling",
+                "count": 13
+            },
+            {
+                "value": "Jo Nesbø",
+                "count": 10
+            },
+            {
+                "value": " Mary GrandPré",
+                "count": 9
+            },
+            {
+                "value": " Don Bartlett",
+                "count": 7
+            },
+            {
+                "value": " J.K. Rowling",
+                "count": 2
+            },
+            {
+                "value": " James  Marshall",
+                "count": 2
+            },
+            {
+                "value": "Harry Allard",
+                "count": 2
+            },
+            {
+                "value": " Andrew Williamson",
+                "count": 1
+            },
+            {
+                "value": " Harry Bliss",
+                "count": 1
+            }
+        ],
+        "language_code": [
+            {
+                "value": "eng",
+                "count": 54
+            },
+            {
+                "value": "en-US",
+                "count": 4
+            },
+            {
+                "value": "nor",
+                "count": 2
+            },
+            {
+                "value": "dan",
+                "count": 1
+            },
+            {
+                "value": "fre",
+                "count": 1
+            }
+        ]
+    },
+    "results": [
+        {
+            "score": 13.904385,
+            "highlights": null,
+            "document": {
+                "id": "3753",
+                "goodreads_book_id": 10,
+                "best_book_id": 10,
+                "work_id": 21457570,
+                "books_count": 6,
+                "isbn": "439827604",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "J.K. Rowling"
+                ],
+                "original_publication_year": 2005,
+                "original_title": "Harry Potter Collection (Harry Potter, #1-6)",
+                "title": "Harry Potter Collection (Harry Potter, #1-6)",
+                "language_code": "eng",
+                "average_rating": 4.0,
+                "ratings_count": 24618,
+                "work_ratings_count": 26274,
+                "work_text_reviews_count": 882,
+                "ratings_1": 203,
+                "ratings_2": 186,
+                "ratings_3": 946,
+                "ratings_4": 3891,
+                "ratings_5": 21048,
+                "image_url": "https://images.gr-assets.com/books/1328867351m/10.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1328867351s/10.jpg"
+            }
+        },
+        {
+            "score": 12.605612,
+            "highlights": null,
+            "document": {
+                "id": "5240",
+                "goodreads_book_id": 2033352,
+                "best_book_id": 2033352,
+                "work_id": 2038062,
+                "books_count": 129,
+                "isbn": "8770530688",
+                "isbn13": "9790000000000.0",
+                "authors": [
+                    "Jo Nesbø"
+                ],
+                "original_publication_year": 2005,
+                "original_title": "Frelseren (Harry Hole, #6)",
+                "title": "The Redeemer (Harry Hole, #6)",
+                "language_code": "dan",
+                "average_rating": 4.0,
+                "ratings_count": 9337,
+                "work_ratings_count": 21357,
+                "work_text_reviews_count": 1561,
+                "ratings_1": 196,
+                "ratings_2": 514,
+                "ratings_3": 3746,
+                "ratings_4": 10080,
+                "ratings_5": 6821,
+                "image_url": "https://images.gr-assets.com/books/1418343566m/2033352.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1418343566s/2033352.jpg"
+            }
+        },
+        {
+            "score": 12.017786,
+            "highlights": null,
+            "document": {
+                "id": "3647",
+                "goodreads_book_id": 6891433,
+                "best_book_id": 23355220,
+                "work_id": 7112263,
+                "books_count": 117,
+                "isbn": "None",
+                "isbn13": "9790000000000.0",
+                "authors": [
+                    "Jo Nesbø"
+                ],
+                "original_publication_year": 2009,
+                "original_title": "Panserhjerte (Harry Hole, #8)",
+                "title": "Panserhjerte (Harry Hole, #8)",
+                "language_code": "nor",
+                "average_rating": 4.0,
+                "ratings_count": 10991,
+                "work_ratings_count": 30903,
+                "work_text_reviews_count": 2370,
+                "ratings_1": 227,
+                "ratings_2": 799,
+                "ratings_3": 5498,
+                "ratings_4": 14278,
+                "ratings_5": 10101,
+                "image_url": "https://images.gr-assets.com/books/1408635770m/6891433.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1408635770s/6891433.jpg"
+            }
+        },
+        {
+            "score": 11.982937,
+            "highlights": null,
+            "document": {
+                "id": "422",
+                "goodreads_book_id": 862041,
+                "best_book_id": 862041,
+                "work_id": 2962492,
+                "books_count": 76,
+                "isbn": "545044251",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "J.K. Rowling"
+                ],
+                "original_publication_year": 1998,
+                "original_title": "Complete Harry Potter Boxed Set",
+                "title": "Harry Potter Boxset (Harry Potter, #1-7)",
+                "language_code": "eng",
+                "average_rating": 4.0,
+                "ratings_count": 190050,
+                "work_ratings_count": 204125,
+                "work_text_reviews_count": 6508,
+                "ratings_1": 1105,
+                "ratings_2": 1285,
+                "ratings_3": 7020,
+                "ratings_4": 30666,
+                "ratings_5": 164049,
+                "image_url": "https://images.gr-assets.com/books/1392579059m/862041.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1392579059s/862041.jpg"
+            }
+        },
+        {
+            "score": 10.853247,
+            "highlights": null,
+            "document": {
+                "id": "1744",
+                "goodreads_book_id": 465226,
+                "best_book_id": 465226,
+                "work_id": 1487876,
+                "books_count": 140,
+                "isbn": "184343217X",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "Jo Nesbø",
+                    " Don Bartlett"
+                ],
+                "original_publication_year": 2000,
+                "original_title": "Rødstrupe (Harry Hole, #3) ",
+                "title": "The Redbreast (Harry Hole, #3)",
+                "language_code": "eng",
+                "average_rating": 3.0,
+                "ratings_count": 49999,
+                "work_ratings_count": 60598,
+                "work_text_reviews_count": 3185,
+                "ratings_1": 1041,
+                "ratings_2": 2668,
+                "ratings_3": 13058,
+                "ratings_4": 26010,
+                "ratings_5": 17821,
+                "image_url": "https://images.gr-assets.com/books/1320540474m/465226.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1320540474s/465226.jpg"
+            }
+        },
+        {
+            "score": 10.696962,
+            "highlights": null,
+            "document": {
+                "id": "5289",
+                "goodreads_book_id": 18142329,
+                "best_book_id": 18142329,
+                "work_id": 21962567,
+                "books_count": 102,
+                "isbn": "307960498",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "Jo Nesbø",
+                    " Don Bartlett"
+                ],
+                "original_publication_year": 2013,
+                "original_title": "Politi (Harry Hole, #10)",
+                "title": "Police (Harry Hole, #10)",
+                "language_code": "eng",
+                "average_rating": 4.0,
+                "ratings_count": 14495,
+                "work_ratings_count": 21499,
+                "work_text_reviews_count": 2028,
+                "ratings_1": 136,
+                "ratings_2": 495,
+                "ratings_3": 2863,
+                "ratings_4": 9010,
+                "ratings_5": 8995,
+                "image_url": "https://images.gr-assets.com/books/1373049226m/18142329.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1373049226s/18142329.jpg"
+            }
+        },
+        {
+            "score": 10.696962,
+            "highlights": null,
+            "document": {
+                "id": "2001",
+                "goodreads_book_id": 7952502,
+                "best_book_id": 7952502,
+                "work_id": 11580916,
+                "books_count": 24,
+                "isbn": "61997811",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "Brian Sibley"
+                ],
+                "original_publication_year": 2010,
+                "original_title": "Harry Potter: Film Wizardry",
+                "title": "Harry Potter: Film Wizardry",
+                "language_code": "eng",
+                "average_rating": 4.0,
+                "ratings_count": 45081,
+                "work_ratings_count": 46063,
+                "work_text_reviews_count": 526,
+                "ratings_1": 1058,
+                "ratings_2": 984,
+                "ratings_3": 4017,
+                "ratings_4": 8553,
+                "ratings_5": 31451,
+                "image_url": "https://images.gr-assets.com/books/1464452934m/7952502.jpg",
+                "small_image_url": "https://images.gr-assets.com/books/1464452934s/7952502.jpg"
+            }
+        },
+        {
+            "score": 10.424129,
+            "highlights": null,
+            "document": {
+                "id": "1417",
+                "goodreads_book_id": 426775,
+                "best_book_id": 426775,
+                "work_id": 2902978,
+                "books_count": 89,
+                "isbn": "743290119",
+                "isbn13": "9780000000000.0",
+                "authors": [
+                    "Lauren Weisberger"
+                ],
+                "original_publication_year": 2008,
+                "original_title": "Chasing Harry Winston",
+                "title": "Chasing Harry Winston",
+                "language_code": "eng",
+                "average_rating": 3.0,
+                "ratings_count": 63342,
+                "work_ratings_count": 65899,
+                "work_text_reviews_count": 2590,
+                "ratings_1": 3964,
+                "ratings_2": 10309,
+                "ratings_3": 23393,
+                "ratings_4": 17306,
+                "ratings_5": 10927,
+                "image_url": "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+                "small_image_url": "https://s.gr-assets.com/assets/nophoto/book/50x75-a91bf249278a81aabab721ef782c4a74.png"
+            }
+        }
+    ]
+}
--- search-website-functions-v4/client/src/pages/Search/Search.js
+++ search-website-functions-v4/client/src/pages/Search/Search.js
@@ -9,6 +9,7 @@ import Facets from '../../components/Facets/Facets';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 import "./Search.css";
+import { searchResponse } from '../../components/TestResponse';
 
 export default function Search() {
   
@@ -30,25 +31,29 @@ export default function Search() {
   useEffect(() => {
     setIsLoading(true);
     setSkip((currentPage-1) * top);
-    const body = {
-      q: q,
-      top: top,
-      skip: skip,
-      filters: filters
-    };
+    setResults(searchResponse.results);
+    setFacets(searchResponse.facets);
+    setResultCount(searchResponse.count);
+    setIsLoading(false);
+    // const body = {
+    //   q: q,
+    //   top: top,
+    //   skip: skip,
+    //   filters: filters
+    // };
 
-    axios.post( '/api/search', body)
-      .then(response => {
-            console.log(JSON.stringify(response.data))
-            setResults(response.data.results);
-            setFacets(response.data.facets);
-            setResultCount(response.data.count);
-            setIsLoading(false);
-        } )
-        .catch(error => {
-            console.log(error);
-            setIsLoading(false);
-        });
+    // axios.post( 'https://witty-hill-0f88f920f.4.azurestaticapps.net/api/search', body)
+    //   .then(response => {
+    //         console.log(JSON.stringify(response.data))
+    //         setResults(response.data.results);
+    //         setFacets(response.data.facets);
+    //         setResultCount(response.data.count);
+    //         setIsLoading(false);
+    //     } )
+    //     .catch(error => {
+    //         console.log(error);
+    //         setIsLoading(false);
+    //     });
     
   }, [q, top, skip, filters, currentPage]);
 
