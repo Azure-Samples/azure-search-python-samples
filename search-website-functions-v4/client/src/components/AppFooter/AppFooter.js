1d8d70c9408075f2cceb51fb1beae862ca9c1cff
alxkai <alex@instalily.ai>
2024-03-26 12:07:37 -0400

    initial commit

--- search-website-functions-v4/client/src/components/AppFooter/AppFooter.js
+++ search-website-functions-v4/client/src/components/AppFooter/AppFooter.js
@@ -6,7 +6,11 @@ export default function AppFooter() {
   return (
       <footer className="footer">
         <hr />
-        &copy; 2023 Microsoft
+        &copy; All brand logos are trademarks of their respective owners.
+        <p />
+        The PartSelect logo is a Registered Trademark of Atlantic Laundry Centres, Ltd.
+        <p />
+        Copyright © 1999-2024, Eldis Group Partnership. All rights reserved.
       </footer>
   );
 };
